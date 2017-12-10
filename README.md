c7a03a9e05f555515b9ea93a8b56872ba2ca7ab6
0DAYanc <dayancbardak2@gmail.com>
2017-12-10 14:45:05 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# ShellcodeToAssembly
+
+## Replace here with your shellcode
+```c
+shellcode = ''
+```
+---------
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)]()
+[![Conda](https://img.shields.io/conda/pn/conda-forge/python.svg)]()
+
+## Installation
+`git clone https://github.com/blacknbunny/ShellcodeToAssembly.git && cd ShellcodeToAssembly/ && chmod +x shellcodetoasm.py && pip install -r requirements.txt`
+
+
+## Modules manual installation
+#### `pip install -r requirements.txt` it can be `pip2 install -r requirements.txt`
+
+## Usage
+`python2 shellcodetoasm.py [returnbit] [architecture]`
+
+## For example
+`python2 shellcodetoasm.py 32 x86`
+
+## Architectures
+### `ARM`
+### `ARM64`
+### `MIPS`
+### `ppc`
+### `X86`
+
+## Return Bit
+### `64`
+### `32`
+
+[![demo](https://asciinema.org/a/bxVxkc9U7FxmzDi34eF3bG2T0.png)](https://asciinema.org/a/bxVxkc9U7FxmzDi34eF3bG2T0?autoplay=1)
---
+++ shellcodetoasm.py
@@ -0,0 +1,66 @@
+from capstone import *
+import time, sys, platform, os
+
+# Change here
+shellcode = '\x31\xf6'
+
+def loading():
+    print "Loading..."
+    for i in range(0, 100):
+        time.sleep(0.1)
+        width = (i + 1) / 4
+        bar = "[" + "#" * width + " " * (25 - width) + "]"
+        sys.stdout.write(u"\u001b[1000D" +  bar)
+        sys.stdout.flush()
+    print
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def holefunc(architecture, mode):
+    options = Cs(architecture, mode)
+    listofinstructions = options.disasm(shellcode, 0x2000)
+    for shtoasm in listofinstructions:
+        print("%x\t%s\t%s" %(shtoasm.address, shtoasm.mnemonic, shtoasm.op_str))
+def error(error):
+    cls()
+    print("Select the bit %s" % (error))
+    sys.exit(1)
+
+if len(sys.argv) != 3:
+    cls()
+    print("Author : https://www.github.com/blacknbunny")
+    print("Usage:\t./shellcodetoasm.py [shellcode] [returnbit] [architecture]")
+    sys.exit(1)
+else:
+    '''cls()
+    loading()'''
+    returnbit = sys.argv[1]
+    arch = sys.argv[2]
+    if returnbit == '64':
+        if arch == 'arm':
+            holefunc(CS_ARCH_ARM, CS_MODE_ARM)
+        elif arch == 'arm64':
+            holefunc(CS_ARCH_ARM64, CS_MODE_ARM)
+        elif arch == 'mips':
+            holefunc(CS_ARCH_MIPS, CS_MODE_MIPS64)
+        elif arch == 'ppc':
+            holefunc(CS_ARCH_PPC, CS_MODE_64)
+        elif arch == 'x86':
+            holefunc(CS_ARCH_X86, CS_MODE_64)
+        else:
+            error("architecture")
+    if returnbit == '32':
+        if arch == 'arm':
+            holefunc(CS_ARCH_ARM, CS_MODE_ARM)
+        if arch == 'arm64':
+            holefunc(CS_ARCH_ARM64, CS_MODE_ARM)
+        if arch == 'mips':
+            holefunc(CS_ARCH_MIPS, CS_MODE_MIPS32)
+        if arch == 'ppc':
+            holefunc(CS_ARCH_PPC, CS_MODE_32)
+        if arch == 'x86':
+            holefunc(CS_ARCH_X86, CS_MODE_32)
+        else:
+            error("architecture")
+    else:
+        error("size")
